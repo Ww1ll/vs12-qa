c5dc7914de30598a90ab49a58f358aca4c986f15
William <109118303+Ww1ll@users.noreply.github.com>
2023-07-14 14:52:35 -0300

    Java-Aula02-Exercicios

---
+++ modulo-01-java/aula-02/Exercicios/src/Exercicio01.java
@@ -0,0 +1,13 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int[] valores = {20, 30, 40};
+        int soma = 0;
+        int media = 0;
+
+        soma = valores[0] + valores[1] + valores[2];
+        media = valores[0] + valores[1] + valores[2]/ 3;
+
+        System.out.println(soma);
+        System.out.println(media);
+    }
+}
